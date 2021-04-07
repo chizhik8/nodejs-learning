90de049e5deacf3ab0f4e688e6c4a57b2634a33e
julia_chizhik <for.yu.ch@gmail.com>
2021-04-07 17:35:37 +0300

    summary

--- LESSON05-Bogdan/example-2/app.js
+++ LESSON05-Bogdan/example-2/app.js
@@ -1,23 +1,30 @@
 const express = require("express");
 const dotenv = require("dotenv");
 dotenv.config();
-const {MongoClient} = require("mongodb");
+const { MongoClient } = require("mongodb"); // импортируем
 
 const app = express();
 const dbURI = process.env.DB_HOST;
 
 const port = process.env.PORT || 3000;
 
-async function run (){
-    const client = await new MongoClient(dbURI, { useUnifiedTopology: true }).connect();
-    const result = await client.db("books_shop").collection("authors").find({vacancyName: 'Java Developer'}).toArray();
-    const updateElement = await client.db("books_shop").collection("authors").find({name: "Author"}, {returnOriginal: false});
-    console.log(updateElement)
+// присоединяем MongoClient к базе данных
+async function run() {
+  const client = await new MongoClient(dbURI, {
+    useUnifiedTopology: true,
+  }).connect();
+  const result = await client
+    .db("books_shop")
+    .collection("authors")
+    .find({ vacancyName: "Java Developer" })
+    .toArray();
+  const updateElement = await client
+    .db("books_shop")
+    .collection("authors")
+    .find({ name: "Author" }, { returnOriginal: false });
+  console.log(updateElement);
 }
 
 run();
-
-
-
 
 app.listen(port);
